ddd9e38c16bef10176e89969a1164cf797ed27dc
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-09-23 12:35:45 +0300

    Update 01_find_the_largest.py

--- 06_exercise_basic_syntax/more_execises/01_find_the_largest.py
+++ 06_exercise_basic_syntax/more_execises/01_find_the_largest.py
@@ -1,9 +1,10 @@
-number = int(input())
-number_as_string = str(number)
-my_list = []
-for i in number_as_string:
-    my_list.append(i)
+# number = input()
+# my_list = []
+# for i in number:
+#     my_list.append(i)
+# my_list.sort(reverse=True)
+# print(''.join(my_list))
 
-my_list.sort(reverse=True)
-print(''.join(my_list))
+my_list = list(input())
+print(''.join(sorted(my_list, reverse=True)))
 
