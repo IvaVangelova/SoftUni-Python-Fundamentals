2077cb1dad03d15c3895bb89312ede020fd2cd5e
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-09-28 14:32:37 +0300

    Update 03_list_statistics.py

---
+++ 11_lists_basics/03_list_statistics.py
@@ -0,0 +1,17 @@
+numbers = int(input())
+positive_list = []
+negative_list = []
+
+for i in range(numbers):
+
+    current_number = int(input())
+
+    if current_number >= 0:
+        positive_list.append(current_number)
+    else:
+        negative_list.append(current_number)
+
+print(positive_list)
+print(negative_list)
+print("Count of positives:", len(positive_list))
+print("Sum of negatives:", sum(negative_list))
