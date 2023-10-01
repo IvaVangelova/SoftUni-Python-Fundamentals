0f1cb6bdc6e2b1d91de3ffb14007d33f6c8e6781
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-10-01 15:01:38 +0300

    Update 05_faro_shuffle.py

--- 12_exercise_lists_basics/05_faro_shuffle.py
+++ 12_exercise_lists_basics/05_faro_shuffle.py
@@ -1,3 +1,18 @@
-deck_of_cards = ([card for card in input().split()])
-left_part = []
-right_part = []
+deck_of_cards = input().split()
+faro_shuffles = int(input())
+
+for i in range(faro_shuffles):
+
+    new_deck_after_shuffles = []
+    split_deck = len(deck_of_cards) // 2
+    left_part = deck_of_cards[:split_deck]
+    right_part = deck_of_cards[split_deck:]
+
+    for shuffle in range(len(left_part)):
+
+        new_deck_after_shuffles.append(left_part[shuffle])
+        new_deck_after_shuffles.append(right_part[shuffle])
+        deck_of_cards = new_deck_after_shuffles
+
+print(new_deck_after_shuffles)
+
