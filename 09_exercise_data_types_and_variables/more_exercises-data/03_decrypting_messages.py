cb0bdc7107b8e6e28efe2ea052260fc771132c56
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-10-01 15:42:11 +0300

    updates

--- 09_exercise_data_types_and_variables/more_exercises-data/03_decrypting_messages.py
+++ 09_exercise_data_types_and_variables/more_exercises-data/03_decrypting_messages.py
@@ -1,9 +1,9 @@
 key = int(input())
 number_of_lines = int(input())
-decryting_word = ''
+decrypting_word = ''
 for i in range(number_of_lines):
     character = ord(input())
     current_sum = key + character
-    decryting_word += chr(current_sum)
+    decrypting_word += chr(current_sum)
 
-print(decryting_word)
+print(decrypting_word)
