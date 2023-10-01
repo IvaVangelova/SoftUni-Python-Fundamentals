a6a84ec8d72d449a1e6fa5ebc050332072ce1d11
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-10-01 15:46:44 +0300

    Update 06_survival_of_the_biggest.py

--- 12_exercise_lists_basics/06_survival_of_the_biggest.py
+++ 12_exercise_lists_basics/06_survival_of_the_biggest.py
@@ -1,15 +1,7 @@
 list_of_integers = ([int(number) for number in input().split()])
 count_numbers_removed = int(input())
-# small = max(list_of_integers)
-
-# for i in range(count_numbers_removed):
-#     for j in list_of_integers:
-#         if list_of_integers[j] < small:
-#             small = list_of_integers[j]
-#     list_of_integers.remove(small)
-# print(list_of_integers)
 
 for i in range(count_numbers_removed):
     list_of_integers.remove(min(list_of_integers))
-
-print(', 'list_of_integers.join())
+list_to_strings = (str(s) for s in list_of_integers)
+print(', '.join(list_to_strings))
