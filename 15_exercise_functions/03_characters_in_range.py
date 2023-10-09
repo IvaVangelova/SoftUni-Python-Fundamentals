ca610fbbfcae40a587550628301ab12befc8662b
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-10-09 11:57:10 +0300

    Update 03_characters_in_range.py

--- 15_exercise_functions/03_characters_in_range.py
+++ 15_exercise_functions/03_characters_in_range.py
@@ -1,2 +1,12 @@
+def string_with_characters(first: str, second: str) -> list:
+    """ Returns a list of string_characters """
+    characters_list = []
+    for character in range(ord(first) + 1, ord(second)):
+        characters_list.append(chr(character))
+    return characters_list
+
+
 first_character = input()
 second_character = input()
+characters_in_list = string_with_characters(first_character, second_character)
+print(" ".join(characters_in_list))
